484d83080c359db95e798d64f284bca385b27a18
gguerrero <guillermo_guerrero@outlook.com>
2023-02-09 20:59:12 -0600

    Initial commit

---
+++ src/main/java/com/gguerrero/util/html/controller/MainController.java
@@ -0,0 +1,19 @@
+package com.gguerrero.util.html.controller;
+
+import com.gguerrero.util.html.model.Example;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MainController {
+    @GetMapping(value="example")
+    public Example getExample() throws Exception{
+        throw new Exception();
+
+//        Example ex = new Example("1", "Titulo", "Esto es solo un ejemplo.");
+//        return ex;
+
+    }
+
+    // TODO: an endpoint to get a html url a tag and back value
+}
---
+++ src/main/java/com/gguerrero/util/html/model/Example.java
@@ -0,0 +1,12 @@
+package com.gguerrero.util.html.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Example {
+    private String id;
+    private String title;
+    private String description;
+}
